d888db030ab3cec74f6ce4920008e148388a3b3a
Srikanth Chenna <srikanth.chenna@acuvate.com>
2026-04-30 18:33:37 +0530

    Exceptions

---
+++ Exception Handling/DemoException.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.Exception_Handling
+{
+    internal class DemoException
+    {
+        public static void Operation()
+        {
+            try
+            {
+                int a = 10;
+                a /= 0;
+            }
+
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("Program Ended");
+            }
+        }
+
+        public static void TryWithMultipleCatch()
+        {
+            try
+            {
+                int a = 0;
+                string s = null;
+                string s1 = "";
+                s1 = s1 + s;
+                a = 100 / a;
+
+                
+            }
+
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Source);
+            }
+           
+
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Usingthrow()
+        {
+            int age = 16;
+
+            if (age < 18)
+            {
+                throw new ArgumentException("Under Aged");
+            }
+        }
+    }
+}
---
+++ Exception Handling/InnerExceptionn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.Exception_Handling
+{
+    internal class InnerExceptionn
+    {
+        public static void InnerException()
+        {
+            try
+            {
+                try
+                {
+                    int x = int.Parse("abc"); // causes FormatException
+                }
+                catch (Exception ex)
+                {
+       
+                    throw new ApplicationException("Error while processing data", ex);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Main Exception: " + ex.Message);
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine("Inner Exception: " + ex.InnerException.Message);
+                }
+
+            }
+        }
+    }
+}
